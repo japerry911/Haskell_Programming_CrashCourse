566edd1c21bbfd0ad2ffd30ce999e1d90a58b03e
japerry911 <jack.e.perry@outlook.com>
2020-07-09 14:24:09 -0600

    Sandbox-one finished for now

--- sandbox-one/app/Main.hs
+++ sandbox-one/app/Main.hs
@@ -1,7 +1,13 @@
 --main = print result
-main = print $ factorial 5
+--main = print $ factorial 5
+main = 
+    let x = 5
+    in print $ myIf (x == 5) "is five" "is not five"
 
 addBrackets s = "[" ++ s ++ "]"
 result = map addBrackets ["one", "two", "three"]
 
 factorial n = if n < 2 then 1 else n * factorial(n - 1)
+
+myIf True thenFunc elseFunc = thenFunc
+myIf False thenFunc elseFunc = elseFunc
